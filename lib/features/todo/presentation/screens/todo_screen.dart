2676e9f45285abcfb02dd1e6d4c136d81a983210
nilenjay <nilenjaysinghsengar2006@gmail.com>
2026-02-23 17:32:17 +0530

    Updated TodoScreen

--- lib/features/todo/presentation/screens/todo_screen.dart
+++ lib/features/todo/presentation/screens/todo_screen.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/features/todo/presentation/bloc/todo_bloc.dart';
+import 'package:todo_app/features/todo/presentation/bloc/todo_event.dart';
 import 'package:todo_app/features/todo/presentation/bloc/todo_state.dart';
 
 
@@ -21,13 +22,29 @@ class TodoScreen extends StatelessWidget {
                 final todos=state.todos;
                 if(todos.isNotEmpty){
                   return ListView.builder(
-                      itemCount: state.todos.length,
-                      itemBuilder: (context,state){
-                        final todo=state.todos[index];
-
+                      itemCount: todos.length,
+                      itemBuilder: (context,index){
+                        final todo= todos[index];
+                        return Card(
+                          child: ListTile(
+                            leading: Checkbox(value: todo.isComplete, onChanged: (_){
+                              context.read<TodoBloc>().add(ToggleTodoStatus(id: todo.id));
+                            }),
+                          ),
+                        );
                       }
-                  )
+                  );
                 }
+                else{
+                  return Center(
+                    child: Text('No List, Start the Journey now!!'),
+                  );
+                }
+              }
+              else{
+                return Center(
+                  child: Text('No List yet, Start the Journey now!!'),
+                );
               }
             }),
       ),
